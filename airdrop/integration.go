3ec5d22ed70a6ca23089d0f70d5182a2644ca3eb
Jeff Yanta <jeff@getcode.com>
2025-06-10 11:50:36 -0400

    Fix GetWelcomeBonusAmount checks against other purchases

--- airdrop/integration.go
+++ airdrop/integration.go
@@ -45,7 +45,7 @@ func (i *Integration) GetWelcomeBonusAmount(ctx context.Context, owner *codecomm
 	}
 	purchase := purchases[0]
 	for _, otherPurchase := range purchases {
-		if otherPurchase.PaymentCurrency != purchase.PaymentCurrency || otherPurchase.PaymentAmount != otherPurchase.PaymentAmount {
+		if otherPurchase.PaymentCurrency != purchase.PaymentCurrency || otherPurchase.PaymentAmount != purchase.PaymentAmount {
 			return 0, "", errors.New("user has multiple account creation with welcome bonus purchases")
 		}
 	}
