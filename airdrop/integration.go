32b98d1fc1891776aa8e3e1f4caf3c5641b57159
Jeff Yanta <jeff@getcode.com>
2025-06-11 13:05:18 -0400

    Disable welcome bonuses

--- airdrop/integration.go
+++ airdrop/integration.go
@@ -2,9 +2,6 @@ package airdrop
 
 import (
 	"context"
-	"errors"
-
-	commonpb "github.com/code-payments/flipcash-protobuf-api/generated/go/common/v1"
 
 	codecommon "github.com/code-payments/code-server/pkg/code/common"
 	codetransaction "github.com/code-payments/code-server/pkg/code/server/transaction"
@@ -25,47 +22,7 @@ func NewIntegration(accounts account.Store, iaps iap.Store) codetransaction.Aird
 	}
 }
 
+// Welcome bonuses have been disabled
 func (i *Integration) GetWelcomeBonusAmount(ctx context.Context, owner *codecommon.Account) (float64, codecurrency.Code, error) {
-	userID, err := i.accounts.GetUserId(ctx, &commonpb.PublicKey{Value: owner.PublicKey().ToBytes()})
-	if err == account.ErrNotFound {
-		return 0, "", nil
-	} else if err != nil {
-		return 0, "", err
-	}
-
-	var allAirdropPurchases []*iap.Purchase
-
-	purchases, err := i.iaps.GetPurchasesByUserAndProduct(ctx, userID, iap.ProductCreateAccountBonusGoogle)
-	switch err {
-	case nil:
-		for _, purchase := range purchases {
-			// Because iOS transitioned to a new product ID after release
-			if purchase.Platform == commonpb.Platform_GOOGLE {
-				allAirdropPurchases = append(allAirdropPurchases, purchase)
-			}
-		}
-	case iap.ErrNotFound:
-	default:
-		return 0, "", err
-	}
-
-	purchases, err = i.iaps.GetPurchasesByUserAndProduct(ctx, userID, iap.ProductCreateAccountBonusApple)
-	switch err {
-	case nil:
-		allAirdropPurchases = append(allAirdropPurchases, purchases...)
-	case iap.ErrNotFound:
-	default:
-		return 0, "", err
-	}
-
-	if len(allAirdropPurchases) == 0 {
-		return 0, "", nil
-	}
-	purchase := allAirdropPurchases[0]
-	for _, otherPurchase := range allAirdropPurchases {
-		if otherPurchase.PaymentCurrency != purchase.PaymentCurrency || otherPurchase.PaymentAmount != purchase.PaymentAmount {
-			return 0, "", errors.New("user has multiple conflicting airdrop iaps")
-		}
-	}
-	return purchase.PaymentAmount, codecurrency.Code(purchase.PaymentCurrency), nil
+	return 0, "", nil
 }
